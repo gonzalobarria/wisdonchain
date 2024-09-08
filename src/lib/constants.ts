78f1deb6e4a4cc9717b0b04fedcdaadb433dbe4b
Gonzalo Barría <gonzalobarriamarchant@gmail.com>
2024-09-08 14:32:28 +0300

    new deploy

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ import { hardhat, morphHolesky } from "wagmi/chains"
 
 export const CONTRACT_ADDRESSES: { [chainId: number]: string } = {
   [hardhat.id]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  [morphHolesky.id]: "0x5E7cD63e11Cf691776994323480Ac9278948A92b",
+  [morphHolesky.id]: "0x7A31fa16458539a91100a4802841b5d0361f4eD1",
 }
 
 export const key = process.env.ENCRIPTION_KEY || ""
